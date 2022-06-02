cf2767b177fa1963d1368d90dfee74e62f69db52
arumdanie <hspwatul@gmail.com>
2022-06-02 14:45:35 +0700

    katalog index dan detail product

--- app/Http/Controllers/Web/Frontend/ProductCategory/ProductCategoryController.php
+++ app/Http/Controllers/Web/Frontend/ProductCategory/ProductCategoryController.php
@@ -10,25 +10,14 @@ class ProductCategoryController extends Controller
 {
     public function index($slug)
     {
-<<<<<<< HEAD
-        $productCategory = ProductCategory::where(['slug' => $slug, 'is_active' => true])->with('productSubCategory.product')->first();
-        //return dd($productCategory);
-        $data = [
-            'title' => $productCategory != null ? $productCategory->name : 'Kategori Tidak Ditemukan',
-            'productCategory' => $productCategory,
-            'products' => $productCategory->productSubCategory != null ? $productCategory->productSubCategory->product : null,
-        ];
-
-        return view('frontend.categories.index', $data);
-=======
         $productCategory = ProductCategory::where(['slug' => $slug, 'is_active' => true])->with('productSubCategory.product.seller')->first();
         $data = [
             'title' => $productCategory != null ? $productCategory->name : 'Kategori Tidak Ditemukan',
             'productCategory' => $productCategory,
-            'products' => $productCategory->productSubCategory != null ? $productCategory->productSubCategory->pluck('product') : null,
+            'products' => $productCategory->productSubCategory != null ? $productCategory->productSubCategory : null,
         ];
+        // return dd($data['products']);
 
         return view('frontend.category.index', $data);
->>>>>>> 44269b54190de2d880d4217d5cb2477abab363dc
     }
 }
